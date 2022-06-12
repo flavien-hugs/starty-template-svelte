385e2cccf3d6d89f354c69ea8df3bb561ac32eeb
flavien-hugs <geekactux@gmail.com>
2022-06-12 19:12:24 +0000

    create and pages

---
+++ src/pages/Feature.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { Link } from 'svelte-routing'
+
+	export let router = {};
+
+    router.path;
+    router.route;
+    router.params;
+
+	let src = '/assets/img/services.jpg'
+</script>
+
+<section class="bg-half-170 d-table w-100" style="background:url({src}) center;">
+	<div class="bg-overlay bg-gradient-overlay"></div>
+	<div class="container">
+		<div class="row mt-5 justify-content-center">
+			<div class="col-12">
+				<div class="title-heading text-center">
+					<small class="text-white-50 mb-1 fw-medium text-uppercase mx-auto">What we offer?</small>
+					<h5 class="heading fw-semibold mb-0 page-heading text-white">Services Agency</h5>
+				</div>
+			</div>
+		</div>
+
+		<div class="position-middle-bottom">
+			<nav aria-label="breadcrumb" class="d-block">
+				<ul class="breadcrumb breadcrumb-muted mb-0 p-0">
+					<li class="breadcrumb-item">
+						<Link to="/">Starty</Link>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</section>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,12 @@
+<script>
+	// component.svelte
+	import Hero from '../components/Hero.svelte';
+	import Start from '../components/Start.svelte';
+	import Partner from '../components/Partner.svelte';
+	import Contact from '../components/Contact.svelte';
+</script>
+
+<Hero/>
+<Partner/>
+<Start/>
+<Contact/>
